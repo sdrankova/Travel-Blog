23b6071683b80a23b5bbcc7fc3d15d3c4af0b8ad
sdrankova <49238512+sdrankova@users.noreply.github.com>
2020-12-10 15:18:25 +0200

    Added STATIC_ROOT

--- travelBlog/settings.py
+++ travelBlog/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     join(BASE_DIR, 'static'),
