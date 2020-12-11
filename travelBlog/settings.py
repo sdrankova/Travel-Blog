4cbecc992a109031c2af65eb75345e44f838fce6
sdrankova <49238512+sdrankova@users.noreply.github.com>
2020-12-11 11:26:08 +0200

    Added django-heroku

--- travelBlog/settings.py
+++ travelBlog/settings.py
@@ -14,6 +14,8 @@ from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -145,3 +147,5 @@ MEDIA_ROOT = join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
